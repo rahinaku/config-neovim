f598bef67426a8d132ec8d9c451ca0732a6ef88b
rahinaku <221756500+rahinaku@users.noreply.github.com>
2025-08-03 12:58:06 +0900

    fix lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -8,13 +8,27 @@ return {
         { "ga", "<cmd>Lspsaga code_action<cr>",           desc = "code action" },
         { "gr", "<cmd>Lspsaga rename<cr>",                desc = "rename" },
         { "ge", "<cmd>Lspsaga show_line_diagnostics<cr>", desc = "show line diagnostics" },
-        { "[e", "<cmd>Lspsaga diagnostic_jump_next<cr>",  desc = "show line diagnostics" },
-        { "e]", "<cmd>Lspsaga diagnostic_jump_prev<cr>",  desc = "show line diagnostics" },
+        { "go", "<cmd>Lspsaga outline<cr>",               desc = "show outline" },
+        { "[e", "<cmd>Lspsaga diagnostic_jump_next<cr>",  desc = "diagnostic_jump_next" },
+        { "e]", "<cmd>Lspsaga diagnostic_jump_prev<cr>",  desc = "diagnostic_jump_prev" },
     },
     dependencies = {
         "nvim-lspconfig",
     },
     config       = function()
-        require("lspsaga").setup({})
+        require("lspsaga").setup({
+            lightbulb = {
+                enable = true,
+                sign = false,
+                debounce = 10,
+                sign_priority = 40,
+                virtual_text = true,
+                enable_in_insert = true,
+                ignore = {
+                    clients = {},
+                    ft = {},
+                },
+            }
+        })
     end,
 }
